9cd98ef5e82553d57a5adbb513e892227340c6a8
somyabiswal <somya.kec@gmail.com>
2019-07-19 02:23:54 +0530

    Add extracted data into excel file

--- index.py
+++ index.py
@@ -1,16 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 from fake_useragent import UserAgent
+from xlsxwriter import Workbook
+import xlrd
 
 # Setting-Up User-Agent
 ua = UserAgent()
 header = {'user-agent': ua.chrome}
 
-review_page = requests.get('https://www.consumerreports.org/cro/a-to-z-index/products/index.htm', headers = header)
-print(review_page.content)
+# Setting up excel file
+Wb = Workbook('scrap_file.xlsx')
+Sh  = Wb.add_worksheet()
 
-soup = BeautifulSoup(review_page.content, 'lxml')
-print(soup)
+
+
+review_page = requests.get('https://www.consumerreports.org/cro/a-to-z-index/products/index.htm', headers = header)
 
 soup = BeautifulSoup(review_page.content, 'lxml')
 
@@ -21,5 +25,28 @@ all_divs = soup.find_all('div', attrs= {'class': "crux-body-copy"})
 
 products = [(div.a.string).replace('\n', '') for div in all_divs]
 
-for product in products:
-    print(product)
+# for product in products:
+#     print(product)
+
+all_links = soup.find_all('div', attrs= {'class': "crux-body-copy"})
+
+links = ["https://www.consumerreports.org"+(link.a['href']) for link in all_links]
+
+
+details = zip(products, links)
+#
+# for key, val in details:
+#     print(key, val)
+
+# Wb = xlrd.open_workbook('scrap_file.xlsx')
+
+
+r = 0
+Sh.write(r, 0, 'Product Name')
+Sh.write(r, 1, 'Link')
+for prod, lin in details:
+    Sh.write(r+1, 0, prod)
+    Sh.write(r+1, 1, lin)
+    r = r +1
+
+Wb.close()
--- test.py
+++ test.py
@@ -1,16 +1,5 @@
-from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
-import requests
+a = 'somya'
 
+p = a.join('india')
 
-ua = UserAgent()
-header = {'user-agent': ua.chrome}
-
-
-print(ua.chrome)
-
-myPage= requests.get('http://www.google.com', headers= header)
-
-
-soup = BeautifulSoup(myPage.content, 'lxml')
-print(soup)
+a.join()
